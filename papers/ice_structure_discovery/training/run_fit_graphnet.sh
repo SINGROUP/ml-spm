2d55853a039c02a878fdb6a02bbbcd23380bffe5
NikoOinonen <nikooin@gmail.com>
2023-11-16 13:22:34 +0200

    Updated training scripts with more information.

--- papers/ice_structure_discovery/training/run_fit_graphnet.sh
+++ papers/ice_structure_discovery/training/run_fit_graphnet.sh
@@ -2,29 +2,51 @@
 
 export OMP_NUM_THREADS=1
 
+# Unlike for the PosNet training, the GraphImgNet at the moment only supports single-GPU training, so we don't use torchrun here.
+# Slightly different parameters where used with the different datasets for training the models in the paper:
+# - AFM-ice-Cu111:
+#   --dataset AFM-ice-Cu111
+#   --data_dir ./dataset_Cu111
+#   --urls_train "Ice-K-{1..10}_train_{0..31}.tar"
+#   --urls_val "Ice-K-{1..10}_val_{0..7}.tar"
+#   --urls_test "Ice-K-{1..10}_test_{0..7}.tar"
+#   --zmin -2.5
+#   --z_lims -2.9 0.5
+#   --epochs 1000
+#   --batch_size 16
+# - AFM-ice-Au111-monolayer:
+#   --dataset AFM-ice-Au111-monolayer
+#   --data_dir ./dataset_Au111-monolayer
+#   --urls_train "Ice-K-{1..10}_train_{0..31}.tar"
+#   --urls_val "Ice-K-{1..8}_val_{0..7}.tar"
+#   --urls_test "Ice-K-{1..10}_test_{0..7}.tar"
+#   --zmin -2.5
+#   --z_lims -2.9 0.5
+#   --epochs 1000
+#   --batch_size 16
 python fit_graphnet.py \
     --run_dir ./fit_graphnet_Cu111 \
-    --dataset AFM-ice-Cu111 \
-    --data_dir ./dataset_Cu111 \
+    --dataset AFM-ice-Au111-bilayer \
+    --data_dir ./dataset_Au111-bilayer \
     --urls_train "Ice-K-{1..10}_train_{0..31}.tar" \
-    --urls_val "Ice-K-{1..5}_val_{0..7}.tar" \
-    --urls_test "Ice-K-{1..10}_test_{0..7}.tar" \
+    --urls_val "Ice-K-{1..8}_val_{0..3}.tar" \
+    --urls_test "Ice-K-{1..10}_test_{0..3}.tar" \
     --random_seed 0 \
     --train True \
     --test True \
     --predict True \
-    --epochs 1000 \
+    --epochs 250 \
     --num_workers 6 \
     --batch_size 32 \
-    --avg_best_epochs 5 \
+    --avg_best_epochs 10 \
     --pred_batches 3 \
     --lr 1e-3 \
-    --zmin -2.9 \
-    --z_lims -3.4 0.5 \
+    --zmin -3.5 \
+    --z_lims -3.5 0.5 \
     --peak_std 0.20 \
     --box_res 0.125 0.125 0.100 \
-    --classes 1 6,14 7,15 8,16 9,17,35 \
-    --class_colors 'w' 'dimgray' 'b' 'r' 'yellowgreen' \
+    --classes 1 8 \
+    --class_colors 'w' 'r' \
     --edge_cutoff 3.0 \
     --afm_cutoff 1.125 \
     --loss_weights 1.0 1.0 \
--- papers/ice_structure_discovery/training/run_fit_posnet.sh
+++ papers/ice_structure_discovery/training/run_fit_posnet.sh
@@ -1,31 +1,57 @@
 #!/bin/bash
 
+# Number of GPUs and the number of samples per batch per GPU (total batch size = N_GPU x BATCH_SIZE).
+# The original training used 32 GPUs divided over 4 nodes with one sample per GPU, but only a single
+# node is used here for simplicity.
+N_GPU=4
+BATCH_SIZE=8
+
+# Number of parallel workers per GPU for loading data from disk.
+N_WORKERS=4
+
 export OMP_NUM_THREADS=1
 
+# Slightly different parameters where used with the different datasets for training the models in the paper:
+# - AFM-ice-Cu111:
+#   --dataset AFM-ice-Cu111
+#   --data_dir ./dataset_Cu111
+#   --urls_train "Ice-K-{1..10}_train_{0..31}.tar"
+#   --urls_val "Ice-K-{1..10}_val_{0..7}.tar"
+#   --urls_test "Ice-K-{1..10}_test_{0..7}.tar"
+#   --z_lims -2.9 0.5
+#   --epochs 1200
+# - AFM-ice-Au111-monolayer:
+#   --dataset AFM-ice-Au111-monolayer
+#   --data_dir ./dataset_Au111-monolayer
+#   --urls_train "Ice-K-{1..10}_train_{0..31}.tar"
+#   --urls_val "Ice-K-{1..5}_val_{0..7}.tar"
+#   --urls_test "Ice-K-{1..10}_test_{0..7}.tar"
+#   --z_lims -2.9 0.5
+#   --epochs 1000
 torchrun \
     --standalone \
     --nnodes 1 \
-    --nproc_per_node 1 \
+    --nproc_per_node $N_GPU \
     --max_restarts 0 \
     fit_posnet.py \
         --run_dir ./fit_posnet_Cu111 \
-        --dataset AFM-ice-Cu111 \
-        --data_dir ./dataset_Cu111 \
+        --dataset AFM-ice-Au111-bilayer \
+        --data_dir ./dataset_Au111-bilayer \
         --urls_train "Ice-K-{1..10}_train_{0..31}.tar" \
-        --urls_val "Ice-K-{1..5}_val_{0..7}.tar" \
-        --urls_test "Ice-K-{1..10}_test_{0..7}.tar" \
+        --urls_val "Ice-K-{1..5}_val_{0..3}.tar" \
+        --urls_test "Ice-K-{1..10}_test_{0..3}.tar" \
         --random_seed 0 \
         --train True \
         --test True \
         --predict True \
-        --epochs 1000 \
-        --num_workers 4 \
-        --batch_size 8 \
+        --epochs 800 \
+        --num_workers $N_WORKERS \
+        --batch_size $BATCH_SIZE \
         --avg_best_epochs 5 \
         --pred_batches 10 \
-        --lr 1e-3 \
-        --zmin -2.9 \
-        --z_lims -3.4 0.5 \
+        --lr 1e-4 \
+        --zmin -10.0 \
+        --z_lims -3.5 0.5 \
         --peak_std 0.20 \
         --box_res 0.125 0.125 0.100 \
         --loss_labels "MSE (pos.)" \
