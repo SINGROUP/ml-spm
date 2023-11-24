f0bd41afb3a0cb0eca48df281440af6578dfcf4e
NikoOinonen <42408893+NikoOinonen@users.noreply.github.com>
2023-11-24 15:01:22 +0200

    Add Github CI test workflow (#28)

---
+++ tests/test_utils.py
@@ -0,0 +1,48 @@
+import os
+import numpy as np
+
+
+def test_xyz_read_write():
+    from mlspm.utils import read_xyzs, write_to_xyz
+
+    xyz_path = "./test.xyx"
+    # fmt: off
+    xyz = np.array([
+        [ 0.0,  0.1,  0.2,  0.3,  1],  # (x, y, z, charge, element)
+        [ 0.0, -0.1, -0.2, -0.3,  6],
+        [-0.2,  1.5, -2.5,  0.2, 14]]
+    )
+    # fmt: on
+
+    write_to_xyz(xyz, xyz_path)
+    xyz_read = read_xyzs([xyz_path])[0]
+
+    os.remove(xyz_path)
+
+    assert np.allclose(xyz, xyz_read)
+
+
+def test_loss_log_plot():
+    from mlspm.logging import LossLogPlot
+
+    log_path = "loss_log.csv"
+    plot_path = "test_plot.png"
+
+    if os.path.exists(log_path):
+        os.remove(log_path)
+    loss_log = LossLogPlot(log_path, plot_path, loss_labels=["1", "2"], loss_weights=["", ""])
+
+    losses = [[[0, 1, 2], [1, 2, 3]], [[1.5, 2.0, 3.0], [0.1, 0.4, 0.7]]]
+    for loss in losses:
+        loss_log.add_train_loss(loss[0])
+        loss_log.add_val_loss(loss[1])
+    loss_log.next_epoch()
+
+    new_log = LossLogPlot(log_path, "plot.png", loss_labels=["1", "2"], loss_weights=["", ""])
+
+    os.remove(log_path)
+    os.remove(plot_path)
+
+    assert new_log.epoch == 2
+    assert np.allclose(new_log.train_losses, np.array([[0.75, 1.5, 2.5]])), new_log.train_losses
+    assert np.allclose(new_log.val_losses, np.array([[0.55, 1.2, 1.85]])), new_log.val_losses
